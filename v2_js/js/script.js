c67fb9cb7ceedca540f4a944f0e897883ab2ec72
rigwild <contact@asauvage.fr>
2018-05-15 03:10:13 +0200

    JS version homepage is done and working

--- v2_js/js/script.js
+++ v2_js/js/script.js
@@ -7,7 +7,11 @@ const corsProxy = "https://cors-anywhere.herokuapp.com/"
 
 const apiBaseUrl = "https://api.appvertical.com/api/"
 
+
 const log = (...ele) => ele.forEach(x => console.log(x))
+
+
+/********** Get the data **********/
 
 const reqAPI = (apiUrl, httpMethod, dataParam, headersParam) => {
 	return new Promise((resolve, reject) => {
@@ -22,9 +26,16 @@ const reqAPI = (apiUrl, httpMethod, dataParam, headersParam) => {
 	})
 }
 
-const login = (vusername, vpassword, returnOnlyTheToken) => {
+const reqLogin = (vusername, vpassword) => {
 	if (!vusername || !vpassword)
 		return
+
+	$("#seriesLocation").html("Logging in ...")
+
+	const token = sessionStorage.getItem("token")
+	if (token && token !== "")
+		return new Promise(res => res(token))
+
 	const dataParam = {
 		login: vusername,
 		password: vpassword
@@ -32,7 +43,7 @@ const login = (vusername, vpassword, returnOnlyTheToken) => {
 	return reqAPI("login/authenticate", "POST", dataParam)
 }
 
-const getHome = authToken => {
+const reqGetHome = authToken => {
 	if (!authToken)
 		return
 	const headersParam = {
@@ -41,8 +52,51 @@ const getHome = authToken => {
 	return reqAPI("playlists?page=1&limit=9999999", "GET", null, headersParam)
 }
 
-const startScript = async (vusername, vpassword) => {
-	login(vusername, vpassword, true)
+const reqGetAllVideos = authToken => {
+	if (!authToken)
+		return
+	const headersParam = {
+		"X-Access-Token": authToken
+	}
+	return reqAPI("videos?limit=9999999", "GET", null, headersParam)
+}
+
+
+
+/********** Start the script **********/
+
+const setupGetSeries = (vusername, vpassword) => {
+	reqLogin(vusername, vpassword)
+		.catch(e => {
+			log(e)
+			$("#seriesLocation").html("Error ! Cant't log in. The access token cache has been reloaded.<br>Try to refresh the page.")
+			sessionStorage.clear()
+			return null
+		})
+		.then(res => {
+			if (!res)
+				return
+			let authToken
+			if (typeof res !== 'string') {
+				authToken = res.token
+				sessionStorage.setItem("token", res.token)
+			} else {
+				authToken = res
+			}
+			$("#seriesLocation").html("Fetching videos from Vertical servers ...")
+			reqGetHome(authToken)
+				.catch(e => {
+					log(e)
+					$("#seriesLocation").html("Error ! Can't fetch the videos. Try to reload the page. If it still doesn't work post an issue on the Github page.")
+					sessionStorage.clear()
+					return null
+				})
+				.then(res => setSeriesToPage(res.results))
+		})
+}
+
+const setupGetVideos = (vusername, vpassword) => {
+	reqLogin(vusername, vpassword)
 		.catch(e => {
 			log(e)
 			return null
@@ -51,11 +105,70 @@ const startScript = async (vusername, vpassword) => {
 			if (!res)
 				return
 			let authToken = res.token
-			getHome(authToken)
+			reqGetAllVideos(authToken)
 				.catch(e => {
 					log(e)
 					return null
 				})
-				.then(res => log(res))
+				.then(res => setSeriesToPage(res.results))
 		})
+}
+
+
+
+
+const setSeriesToPage = series => {
+	if (!series)
+		return
+	$("#seriesLocation").html("")
+	series.forEach(oneSeries => {
+		let oneSeriesString = `<div class='series'><span class="seriesTitle">${oneSeries.name}</span><div class='seriesVideos'>`
+
+		oneSeries.videos.forEach(aVideo => {
+			oneSeriesString += `<a href='watch.html?id=${aVideo._id}'>
+					<div class='overlay'></div>
+					<div class='overlayContent' id="videoName">${aVideo.name}</div>
+					<img src='${aVideo.images.thumbnail.url}' /></a>`
+		})
+		oneSeriesString += `</div></div>`
+		$("#seriesLocation").html($("#seriesLocation").html() + oneSeriesString)
+	})
+	initVideoStyle()
+}
+
+
+
+
+const initVideoStyle = () => {
+	const fadeTitle = true
+	const fadeTime = 200
+
+	$(".seriesVideos a").mouseenter(function() {
+		if (fadeTitle) {
+			$(this).children(".overlay").fadeIn({
+				queue: false,
+				duration: fadeTime
+			}, 'linear')
+			$(this).children(".overlayContent").fadeIn({
+				queue: false,
+				duration: fadeTime
+			}, 'linear')
+		} else {
+			$(this).children(".overlay").show()
+			$(this).children(".overlayContent").show()
+		}
+	})
+	$(".seriesVideos a").mouseleave(function() {
+		if (fadeTitle) {
+			$(this).children(".overlay").fadeOut({
+				duration: fadeTime
+			}, 'linear')
+			$(this).children(".overlayContent").fadeOut({
+				duration: fadeTime
+			}, 'linear')
+		} else {
+			$(this).children(".overlay").hide()
+			$(this).children(".overlayContent").hide()
+		}
+	})
 }
